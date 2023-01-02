ecbb41365ede806b61633557876ed3e86dd60c98
GORGEOUS💞 <oncwnuJ6kAZRcg-MLBKhZYh8r0Ds@git.weixin.qq.com>
2023-01-02 22:27:25 +0800

    add the dot of tabbarview

--- frontend/fanfan/src/views/Tabbar.vue
+++ frontend/fanfan/src/views/Tabbar.vue
@@ -3,9 +3,10 @@
         <router-view></router-view>
     </div>
     <div class="tabbar">
-        <nut-tabbar :bottom="true" :safe-area-inset-bottom="true" @tab-switch="tabSwitch" v-model:visible="activeName">
+        <nut-tabbar :bottom="true" :safe-area-inset-bottom="true" @tab-switch="tabSwitch" v-model:visible="activeName"
+            unactive-color="#7d7e80" active-color="#e23c13">
             <nut-tabbar-item to="/more" tab-title="more" name="category" icon="category"></nut-tabbar-item>
-            <nut-tabbar-item to="/home" tab-title="home" name="home" icon="home"></nut-tabbar-item>
+            <nut-tabbar-item to="/home" tab-title="home" name="home" icon="home" :dot="homedotflag"></nut-tabbar-item>
             <nut-tabbar-item to="/liked" tab-title="liked" name="cart" icon="heart"></nut-tabbar-item>
         </nut-tabbar>
     </div>
@@ -14,31 +15,38 @@
 <script>
 import { ref } from 'vue';
 export default {
-    data(){
+    data() {
+        return {
+            homedotflag: true,
+            activeName: 'home',
+        }
     },
     setup() {
-        const activeName = ref("home");
+        // const activeName = ref("home");
         function tabSwitch(item, index) {
             console.log('item:', item, 'index:', index);
+            this.activeName = index;
         }
         return {
             tabSwitch,
-            activeName,
+            // activeName,
         };
     },
 }
 </script>
 
 <style>
-.top{
-    max-height:calc(100% - 50px);
-    height:calc(100% - 50px);
+.top {
+    max-height: calc(100% - 50px);
+    height: calc(100% - 50px);
 }
-.tabbar{
+
+.tabbar {
     display: flex;
-    height:50px;
+    height: 50px;
 }
-.nut-tabbar-bottom{
-    position:absolute;
+
+.nut-tabbar-bottom {
+    position: absolute;
 }
 </style>
