f417ab6b0356c49d222e667e8c8e17b6eafc2161
CLIMOiiin <1309824959@qq.com>
2023-01-03 22:56:26 +0800

    文档更新

--- frontend/fanfan/src/views/FanFanView.vue
+++ frontend/fanfan/src/views/FanFanView.vue
@@ -11,9 +11,9 @@
         <nut-tabpane title="待评价">
             <nut-card :img-url="state.list[0].imgUrl" :title="state.list[0].title" :price="state.list[0].price"
                 :shopName="state.list[0].shopName">
-                <template #prolist>
+                <!-- <template #prolist>
                     <nut-tag color="#E9E9E9" textColor="#999999" plain>微辣</nut-tag>
-                </template>
+                </template> -->
                 <template #origin>
                 </template>
                 <template #shop-tag>
@@ -26,85 +26,22 @@
                     </div>
                 </template>
             </nut-card>
-            <nut-card :img-url="state.list[1].imgUrl" :title="state.list[1].title" :price="state.list[1].price"
-                :shopName="state.list[1].shopName">
-                <template #prolist>
-                    <div class="customize">不辣</div>
-                </template>
-                <!-- <template #price>
-                    <div class="customize">价格</div>
-                </template> -->
-                <template #origin>
-                    <div class="customize"></div>
-                </template>
-                <template #shop-tag>
-                    <div class="customize">大食堂二楼</div>
-                </template>
-                <template #footer>
-                    <div class="customize">
-                        <nut-button type="primary" @click="Comment()">评价</nut-button>
-                    </div>
-                </template>
-            </nut-card>
-            <nut-card :img-url="state.list[2].imgUrl" :title="state.list[2].title" :price="state.list[2].price"
-                :shopName="state.list[2].shopName">
-                <template #prolist>
-                    <div class="customize">不辣</div>
-                </template>
-                <!-- <template #price>
-                    <div class="customize">价格</div>
-                </template> -->
-                <template #origin>
-                    <div class="customize"></div>
-                </template>
-                <template #shop-tag>
-                    <div class="customize">大食堂二楼</div>
-                </template>
-                <template #footer>
-                    <div class="customize">
-                        <nut-button type="primary" @click="Comment()">评价</nut-button>
-                    </div>
-                </template>
-            </nut-card>
-            <nut-card :img-url="state.list[3].imgUrl" :title="state.list[3].title" :price="state.list[3].price"
-                :shopName="state.list[3].shopName">
-                <template #prolist>
-                    <div class="customize">不辣</div>
-                </template>
-                <!-- <template #price>
-                    <div class="customize">价格</div>
-                </template> -->
-                <template #origin>
-                    <div class="customize"></div>
-                </template>
-                <template #shop-tag>
-                    <div class="customize">大食堂二楼</div>
-                </template>
-                <template #footer>
-                    <div class="customize">
-                        <nut-button type="primary" @click="Comment()">评价</nut-button>
-                    </div>
-                </template>
-            </nut-card>
         </nut-tabpane>
         <nut-tabpane title="已评价">
             <nut-card :img-url="state.list[0].imgUrl" :title="state.list[0].title" :price="state.list[0].price"
                 :shopName="state.list[0].shopName">
-                <template #prolist>
-                    <div class="customize">不辣</div>
-                </template>
-                <!-- <template #price>
-                    <div class="customize">价格</div>
+                <!-- <template #prolist>
+                    <nut-tag color="#E9E9E9" textColor="#999999" plain>微辣</nut-tag>
                 </template> -->
                 <template #origin>
-                    <div class="customize"></div>
                 </template>
                 <template #shop-tag>
-                    <div class="customize">大食堂二楼</div>
+                    <nut-rate active-color="#FFC800" v-model="state.list[0].rate" readonly spacing="10" style="height:20px" />
+                    <div style="color: gray;font-size: small;">{{ state.list[0].rate }}</div>
                 </template>
                 <template #footer>
                     <div class="customize">
-                        <nut-button type="primary" @click="Comment()">查看</nut-button>
+                        <nut-button type="mini" @click="Comment()">评价</nut-button>
                     </div>
                 </template>
             </nut-card>
