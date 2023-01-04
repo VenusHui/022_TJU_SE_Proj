b8186b16616d085be6a4520e38aa2c7c34af2433
GORGEOUS💞 <oncwnuJ6kAZRcg-MLBKhZYh8r0Ds@git.weixin.qq.com>
2023-01-04 13:01:37 +0800

    修改likedview

--- frontend/fanfan/src/views/LikedView.vue
+++ frontend/fanfan/src/views/LikedView.vue
@@ -8,15 +8,26 @@
         <div style="margin-top:10px">
             <!-- <nut-list :listData="likeditems" height="260" container-height="530"> -->
             <nut-list :listData="likedlist" height="260" container-height="530">
-                <template v-slot:default="{ index, item }">
+                <template v-slot:default="{ item }">
                     <nut-row class="row" type="flex" justify="center" gutter="10">
+                        <nut-col :span="11">
+                            <LikedCard :name="item.left.name" :imgUrl="item.left.url" :_id="item.left._id">
+                            </LikedCard>
+                        </nut-col>
+                        <nut-col :span="11">
+                            <LikedCard :name="item.right.name" :imgUrl="item.right.url" :_id="item.right._id">
+                            </LikedCard>
+                        </nut-col>
+                    </nut-row>
+                    <!-- <nut-row v-if="index % 2 == 0" class="row" type="flex" justify="center" gutter="10">
                         <nut-col :span="11">
                             <LikedCard :name="item.name" :imgUrl="item.url" :_id="item._id"></LikedCard>
                         </nut-col>
                         <nut-col :span="11">
-                            <LikedCard :name="likedlist[index + 1].name" :imgUrl="likedlist[index + 1].url" :_id="likedlist[index + 1]._id"></LikedCard>
+                            <LikedCard :name="likedlist[index + 1].name" :imgUrl="likedlist[index + 1].url"
+                                :_id="likedlist[index + 1]._id"></LikedCard>
                         </nut-col>
-                    </nut-row>
+                    </nut-row> -->
                 </template>
             </nut-list>
         </div>
@@ -24,7 +35,6 @@
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue';
 import LikedCard from '@/components/LikedCard.vue';
 import axios from 'axios'
 export default {
@@ -35,25 +45,41 @@ export default {
         return {
             likedlist: [
                 {
-                    name: '同济月饼',
-                    url: 'http://rntej2yad.hd-bkt.clouddn.com/assets/dish15.jpg',
-                    _id:'63b40092397b4e6e97d5b2f5'
+                    left: {
+                        name: '同济月饼',
+                        url: 'http://rntej2yad.hd-bkt.clouddn.com/assets/dish15.jpg',
+                        _id: '63b40092397b4e6e97d5b2f5'
+                    },
+                    right: {
+                        name: '无骨鸡爪',
+                        url: 'https://th.bing.com/th/id/OIP.QlzawUNOCC48hH-1paUAygHaLH?pid=ImgDet&rs=1',
+                        _id: '63b43b264215fc121e4f476f'
+                    },
                 },
                 {
-                    name: '兰州牛肉面',
-                    url: 'http://rntej2yad.hd-bkt.clouddn.com/assets/dish10.jpg',
-                    _id:'63b43af84215fc121e4f476d'
+                    left: {
+                        name: '兰州牛肉面',
+                        url: 'http://rntej2yad.hd-bkt.clouddn.com/assets/dish10.jpg',
+                        _id: '63b43af84215fc121e4f476d'
+                    },
+                    right: {
+                        name: '济不理蒸包',
+                        url: 'https://img.zcool.cn/community/01a1e759be4dbda8012075340dcb6f.jpg@1280w_1l_2o_100sh.jpg',
+                        _id: '63b49c094215fc121e4f4778'
+                    },
                 },
                 {
-                    name: '无骨鸡爪',
-                    url: 'https://th.bing.com/th/id/OIP.QlzawUNOCC48hH-1paUAygHaLH?pid=ImgDet&rs=1',
-                    _id:'63b43b264215fc121e4f476f'
+                    left: {
+                        name: '鸡公煲',
+                        url: 'http://rntej2yad.hd-bkt.clouddn.com/assets/dish03.jpg',
+                        _id: '63b49d8e4215fc121e4f477b'
+                    },
+                    right: {
+                        name: '猪肚鸡',
+                        url: 'http://rntej2yad.hd-bkt.clouddn.com/assets/dish02.jpg',
+                        _id: '63b49dc74215fc121e4f477c'
+                    },
                 },
-                {
-                    name: '济不理蒸包',
-                    url: 'https://img.zcool.cn/community/01a1e759be4dbda8012075340dcb6f.jpg@1280w_1l_2o_100sh.jpg',
-                    _id:'63b49c094215fc121e4f4778'
-                }
             ],
             // likeditems: [
             //     {
